a53663280de2700115cfbf3eb8651cebfe50f9ba
ashkrit@gmail.com <Crack@it12>
2023-08-27 23:40:10 +0800

    Embedding Example

---
+++ embedding/sentence-embedding.py
@@ -0,0 +1,16 @@
+from sentence_transformers import SentenceTransformer
+
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+sentences = ['This framework generates embeddings for each input sentence',
+    'Sentences are passed as a list of string.',
+    'The quick brown fox jumps over the lazy dog.']
+
+embeddings = model.encode(sentences)
+
+#Print the embeddings
+for sentence, embedding in zip(sentences, embeddings):
+    print("Sentence:", sentence)
+    print("Embedding:", embedding)
+    print("")    
