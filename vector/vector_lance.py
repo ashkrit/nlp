1fbaa8c89090606f3b8357db0fab8cfbe5be2232
ashkrit@gmail.com <Crack@it12>
2023-08-27 18:22:11 +0800

    Vector Db example

---
+++ vector/vector_lance.py
@@ -0,0 +1,20 @@
+import lancedb
+
+uri = "~/_data/vector/data/sample-lancedb"
+db = lancedb.connect(uri)
+
+tnames = db.table_names()
+
+
+
+table = db.open_table("my_table")
+
+#table = db.create_table("my_table",
+#                         data=[{"vector": [3.1, 4.1], "item": "foo", "price": 10.0},
+ #                              {"vector": [5.9, 26.5], "item": "bar", "price": 20.0}])
+
+result = table.search([100, 100]).limit(2).to_df()
+
+
+
+print(result)
