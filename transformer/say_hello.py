2320c37190b8809235fa1818d4bd6996712d3d66
ashkrit@gmail.com <Crack@it12>
2023-09-09 15:30:40 +0800

    Commonly used models

---
+++ transformer/say_hello.py
@@ -0,0 +1,52 @@
+from transformers import pipeline
+import pandas as pd
+
+
+text = """Dear Amazon, last week I ordered an Optimus Prime action figure \
+from your online store in Germany. Unfortunately, when I opened the package, \
+I discovered to my horror that I had been sent an action figure of Megatron \
+instead! As a lifelong enemy of the Decepticons, I hope you can understand my \
+dilemma. To resolve the issue, I demand an exchange of Megatron for the \
+Optimus Prime figure I ordered. Enclosed are copies of my records concerning \
+this purchase. I expect to hear from you soon. Sincerely, Bumblebee."""
+
+
+classifier = pipeline("text-classification")
+
+outputs = classifier(text)
+print(pd.DataFrame(outputs))    
+
+
+## Name Entity 
+ner_tagger = pipeline("ner", aggregation_strategy="simple")
+outputs = ner_tagger(text)
+print(pd.DataFrame(outputs))   
+
+
+## Question and Answer 
+
+reader = pipeline("question-answering")
+question = "what does customer want"
+outputs = reader(question=question, context=text)
+print(pd.DataFrame([outputs]))
+
+
+## Summary 
+
+summarizer = pipeline("summarization")
+outputs = summarizer(text, max_length=70, clean_up_tokenization_spaces=True)
+print(outputs[0]['summary_text'])
+
+## Language Translation - English to German
+translator = pipeline("translation_en_to_de",model="Helsinki-NLP/opus-mt-en-de")
+outputs = translator(text, clean_up_tokenization_spaces=True, min_length=100)
+print(outputs[0]['translation_text'])
+
+
+
+## Reply via text generation
+generator = pipeline("text-generation")
+response = "Dear Bumblebee, I am sorry to hear that your order was mixed up."
+prompt = text + "\n\nCustomer service response:\n" + response
+outputs = generator(prompt, max_length=200)
+print(outputs[0]['generated_text'])
