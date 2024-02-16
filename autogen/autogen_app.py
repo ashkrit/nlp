99cebcd40d6375403fe337bf7b52cdb3225f7b43
ashkrit@gmail.com <Crack@it12>
2024-02-16 20:36:18 +0800

    Update git ignore

--- autogen/autogen_app.py
+++ autogen/autogen_app.py
@@ -11,8 +11,10 @@ config_list = [
 
 llm_config = {"config_list" : config_list}
 
-assistant = autogen.AssistantAgent("assistant",llm_config = llm_config)
+assistant = autogen.AssistantAgent("assistant",llm_config = llm_config,system_message="You are helpfull assistant")
+
 user_proxy = autogen.UserProxyAgent("user_proxy",code_execution_config = {"use_docker":False})
+
 user_proxy.initiate_chat(assistant,message ="What is the name of the model you are based on?")
 
 
