6e2a56104230cf8eb3e80664d34f79602cca6069
ashkrit@gmail.com <Crack@it12>
2024-02-16 20:18:21 +0800

    Add auto gen to ollama

---
+++ autogen/autogen_app.py
@@ -0,0 +1,18 @@
+import autogen
+
+
+config_list = [
+    {
+        "model":"llama2",
+        "base_url": "http://127.0.0.1:11434/v1",
+        "api_key" : "ollama",
+    }
+]
+
+llm_config = {"config_list" : config_list}
+
+assistant = autogen.AssistantAgent("assistant",llm_config = llm_config)
+user_proxy = autogen.UserProxyAgent("user_proxy",code_execution_config = {"use_docker":False})
+user_proxy.initiate_chat(assistant,message ="What is the name of the model you are based on?")
+
+
