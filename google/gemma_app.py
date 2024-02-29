d3fbf86ed9b67c7a65456dd84bb4c41debb85d9d
ashkrit@gmail.com <Crack@it12>
2024-02-29 08:32:51 +0800

    Example of Auto GPT and Gemma model

--- google/gemma_app.py
+++ google/gemma_app.py
@@ -1,25 +1,23 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import sys
+import os 
 
-tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b-it")
-model = AutoModelForCausalLM.from_pretrained("google/gemma-7b-it")
 
-system_prompt = "You are a helpful assistant."
+os.environ ['HF_HUB_ENABLE_HF_TRANSFER'] = '1'
+model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", # 'google/gemma-2b-it
+                                    )
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
 
-while True:
-    text = input("question:")
+chat = [
+    { "role": "user", "content": "What is the difference between LLaMAs, Alpacas, and Vicunas" },
+]
 
-    messages = [
+prompt = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
+print(prompt)
 
-        {"role": "user", "content": system_prompt + "\n\n" + text}
-    ]
+inputs = tokenizer.encode(prompt, add_special_tokens=True, return_tensors="pt")
+outputs = model.generate(input_ids=inputs.to(model.device),max_new_tokens=512)
 
-    prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+text = tokenizer.decode(outputs[0],skip_special_tokens=True, clean_up_tokenization_spaces=True)
 
-    print("Prompt " + prompt)
-
-    input = tokenizer.encode(prompt, add_special_tokens=True, return_tensors="pt")
-    outputs = model.generate(input_ids = input.to(model.device), max_length=200, 
-                             do_sample=True, top_k=50, top_p=0.95, temperature=0.8)
-    print("Answer -> ")
-    print(tokenizer.decode(outputs[0]))
+print(text)
