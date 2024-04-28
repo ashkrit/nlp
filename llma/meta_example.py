73a92f62e488fb5e8715f637578b6811428cd67f
ashkrit@gmail.com <ashkrit@gmail.com>
2024-04-28 17:59:50 +0800

    Panda AI example

--- llma/meta_example.py
+++ llma/meta_example.py
@@ -1,6 +1,9 @@
 from meta_ai_api import MetaAI
+from requests_html import HTMLSession
 
 ai = MetaAI()
 
+t = ai.get_access_token()
+print(t)
 response = ai.prompt(message="When ios next match in IPL")
 print(response)
