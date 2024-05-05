299781b6c15ab1184d3474ac3bc35bb49b246e19
ashkrit@gmail.com <ashkrit@gmail.com>
2024-05-05 10:39:22 +0800

    Image

--- replicate/replicate_llama2.py
+++ replicate/replicate_llama2.py
@@ -8,23 +8,7 @@ import requests
 from PIL import Image
 
 key = os.getenv('replit_key')
-
 os.environ["REPLICATE_API_TOKEN"] = key
-
-
-
-llm = Replicate(
-    model="a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
-    model_kwargs={"temperature": 0.75, "max_length": 500, "top_p": 1},
-)
-prompt = """
-User: Answer the following yes/no question by reasoning step by step. Can a dog drive a car?
-Assistant:
-"""
-reply = llm(prompt)
-
-print(reply)
-
 
 text2image = Replicate(
     model="stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
@@ -32,10 +16,16 @@ text2image = Replicate(
 )
 
 
-image_output = text2image("A cat riding a motorcycle by Picasso")
-print(image_output)
 
-response = requests.get(image_output)
-img = Image.open(BytesIO(response.content))
+## read text from console 
 
-img
+while True:
+    text = input("Enter text: ")
+    if text == "exit":
+        break
+    image_output = text2image(text)
+    print(f"Path {image_output}")
+    response = requests.get(image_output)
+    img = Image.open(BytesIO(response.content))
+    img.show()    
+
