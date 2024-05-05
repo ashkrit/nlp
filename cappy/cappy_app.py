2a513010131c16166480ea1e2bdb10f9b35a4b91
ashkrit@gmail.com <ashkrit@gmail.com>
2024-05-05 22:53:42 +0800

    Extract function

--- cappy/cappy_app.py
+++ cappy/cappy_app.py
@@ -3,6 +3,12 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 tokenizer = AutoTokenizer.from_pretrained('btan2/cappy-large')
 cappy = AutoModelForSequenceClassification.from_pretrained('btan2/cappy-large')
 
+## new function to calluclate score based on instruction and response
+def calculate_score(instruction, response) -> float:
+    inputs = tokenizer([(instruction, response), ], return_tensors='pt')
+    score = cappy(**inputs).logits[0][0].item()
+    return score
+
 instruction = """
 What label best describes this news article?
 Carlyle Looks Toward Commercial Aerospace (Reuters) Reuters - Private investment firm Carlyle Group,
@@ -12,7 +18,5 @@ its bets on another part of the market.
 """
 response = 'business'
 
-inputs = tokenizer([(instruction, response), ], return_tensors='pt')
-score = cappy(**inputs).logits[0][0].item()
-
+score = calculate_score(instruction, response)
 print(score)
